ceed395273b411889dd676dbb5cef44a27565ace
Willem Khoo <willem.a.khoo@gmail.com>
2025-10-20 15:03:31 -0400

    Initial commit

---
+++ src/main/java/com/freereal/SkyHudClient.java
@@ -0,0 +1,10 @@
+package com.freereal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyHudClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
